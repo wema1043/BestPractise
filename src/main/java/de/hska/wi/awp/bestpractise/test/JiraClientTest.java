9af591c4bb3390a8848417a3e3ddc3d46e85123e
mihaisava <mihaisava@yahoo.com>
2015-05-15 19:26:49 +0200

    JUnit Test für JiraClient und JsonParser
    Change-Id: Ie89fd51443931aef13d896d47b2fc72783a16c35

--- src/main/java/de/hska/wi/awp/bestpractise/test/JiraClientTest.java
+++ src/main/java/de/hska/wi/awp/bestpractise/test/JiraClientTest.java
@@ -1,5 +1,19 @@
 package de.hska.wi.awp.bestpractise.test;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import de.hska.wi.awp.bestpractise.service.JiraClient;
+import de.hska.wi.awp.bestpractise.utils.DataStore;
+import de.hska.wi.awp.bestpractise.utils.JsonParser;
+
 public class JiraClientTest {
 
+	@Test
+	public void checkJiraAPIDatenModel() throws Exception{
+		JiraClient client = new JiraClient();
+		JsonParser parser = new JsonParser();
+		parser.ParseJsonToIssue(client.getAllIssues());
+		assertTrue("Issues können nicht gefunden werden", DataStore.getInstance().getAllIssues.size()>0);
+	}
 }
---
+++ src/main/java/de/hska/wi/awp/bestpractise/test/JsonParserTest.java
@@ -0,0 +1,40 @@
+package de.hska.wi.awp.bestpractise.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import de.hska.wi.awp.bestpractise.utils.Constants;
+import de.hska.wi.awp.bestpractise.utils.DataStore;
+import de.hska.wi.awp.bestpractise.utils.JsonParser;
+import static org.junit.Assert.*;
+
+public class JsonParserTest {
+	
+	@Test
+	public void parseJsonToIssue() throws Exception{
+		//get string for two issues - hard coded
+		String pathToFile = Constants.PATH_CACHE_FOLDER + "/" + "JiraTest" + "/"
+				+ "Jira_2IssuesTest.txt";
+		File file = new File(pathToFile);
+		
+		String json2IssuesAsString = "";
+		if (file.exists()) {
+			InputStream is = new FileInputStream(pathToFile);
+			json2IssuesAsString = IOUtils.toString(is);
+		}
+		
+		//call method parseJsonToIssue
+		JsonParser parser = new JsonParser();
+		parser.ParseJsonToIssue(json2IssuesAsString);
+		
+		//verify the results
+		assertSame("Sind nicht genau 2 Issues erzeugt!", 2, DataStore.getInstance().getAllIssues.size());
+		
+	}
+
+}
