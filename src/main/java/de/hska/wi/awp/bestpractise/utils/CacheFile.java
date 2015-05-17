501dd350c07b3ebd271be3c959ea82b57e2ec24c
Marc Weisenburger <weisenburger.marc@gmail.com>
2015-05-17 18:16:47 +0200

    created testPackage

--- src/main/java/de/hska/wi/awp/bestpractise/utils/CacheFile.java
+++ src/main/java/de/hska/wi/awp/bestpractise/utils/CacheFile.java
@@ -4,9 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-
-import javax.ws.rs.Path;
 
 public class CacheFile {
 	
