16afc5b2f096af3fa60c60ee9e0d0b0bbe8e8913
mihaisava <mihaisava@yahoo.com>
2015-05-19 16:54:03 +0200

    Adapt constants for junit tests
    Change-Id: Ic3200b06ad4c58b6fdd282c8438deab43538d0b6

--- src/main/java/de/hska/wi/awp/bestpractise/utils/Constants.java
+++ src/main/java/de/hska/wi/awp/bestpractise/utils/Constants.java
@@ -12,10 +12,10 @@ package de.hska.wi.awp.bestpractise.utils;
 public class Constants {
 
 	public static final String USER_AGENT = "Mozilla/5.0";
-	public static final String USERNAME = "";
-	public static final String PASSWORD = "";
+	public static final String USERNAME = "saio1011";
+	public static final String PASSWORD = "Rbsramada234";
 	public static final String JIRA_URL_ALL_ISSUES = "http://www.iwi.hs-karlsruhe.de/awpjira/rest/api/2/search?jql=project=HWB&maxResults=-1";
-	public static final String PATH_CACHE_FOLDER = "/Users/Marc/Documents/Liferay/liferay-portal-6.2-ce-ga4/cacheData"; 
-
+//	public static final String PATH_CACHE_FOLDER = "/Applications/liferay-portal-6.2-ce-ga4/cacheData"; 
+	public static final String PATH_CACHE_FOLDER = "/Users/Shared/Jenkins/Home/workspace/BestPracticePortlet";
 	
 }
